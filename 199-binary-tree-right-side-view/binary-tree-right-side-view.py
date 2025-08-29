c22c12d55155f2df3b61c7a117705ebaa579d9bf
Abinash Nagendran <abinashnagendran@gmail.com>
2025-08-29 19:59:14 -0400

    Time: 0 ms (100.00%) | Memory: 12.6 MB (2.18%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        ans = []
+        if root == None:
+            return ans
+        # need breadth first search
+
+        q = deque([root])
+        
+        while q:
+            ans.append(q[-1].val)
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left != None:
+                    q.append(node.left)
+                if node.right != None:
+                    q.append(node.right)
+        return ans
+
+        
