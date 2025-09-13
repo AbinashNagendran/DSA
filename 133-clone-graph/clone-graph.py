a277b6b41f98233a16962b0a53497681b15c890d
Abinash Nagendran <abinashnagendran@gmail.com>
2025-09-12 22:52:33 -0400

    Time: 40 ms (21.47%) | Memory: 12.7 MB (57.06%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        ref = {} # map nodes to cloned nodes
+
+        def dfs(node):
+
+            if node in ref: 
+                return ref[node]
+
+            copy = Node(node.val)
+            ref[node] = copy
+            for neighbour in node.neighbors: 
+                copy.neighbors.append(dfs(neighbour))
+            return copy
+        
+        return dfs(node) if node else None
+        
